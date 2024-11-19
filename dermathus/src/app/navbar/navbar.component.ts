d1d1001cb3cbbd336d9e7c0852db9c91f93829ce
Bianor Araujo <bianor.araujo@gmail.com>
2024-11-19 11:43:31 +0000

    Adjustment on navbar to support mobile

--- dermathus/src/app/navbar/navbar.component.ts
+++ dermathus/src/app/navbar/navbar.component.ts
@@ -1,23 +1,29 @@
 import { Component, HostListener } from '@angular/core';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+	selector: 'app-navbar',
+	templateUrl: './navbar.component.html',
+	styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  isScrolled = false;
-  logoSize = '250px';
+  	isScrolled = false;
+  	logoSize = '250px';
+	isNavbarOpen = false;
 
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    const scrollPosition = window.scrollY || document.documentElement.scrollTop;
-    if (scrollPosition > 50) {
-      this.isScrolled = true;
-      this.logoSize = '130px'; // Reduced logo size
-    } else {
-      this.isScrolled = false;
-      this.logoSize = '250px'; // Original logo size
-    }
-  }
+  	@HostListener('window:scroll', [])
+	
+	onWindowScroll() {
+	const scrollPosition = window.scrollY || document.documentElement.scrollTop;
+		if (scrollPosition > 50) {
+			this.isScrolled = true;
+			this.logoSize = '130px'; // Reduced logo size
+		} else {
+			this.isScrolled = false;
+			this.logoSize = '250px'; // Original logo size
+		}
+	}
+
+	toggleNavbar() {
+		this.isNavbarOpen = !this.isNavbarOpen;
+	}
 }
