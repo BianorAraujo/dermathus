57b65b64826a2e6687985af1d5a2199a690a37c4
Bianor Araujo <bianor.araujo@gmail.com>
2025-02-10 22:00:20 +0000

    Repository refactore

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ServiceComponent } from './service/service.component';
+import { BookingComponent } from './booking/booking.component';
+import { ContactComponent } from './contact/contact.component';
+import { CalendarComponent } from './calendar/calendar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ServiceComponent,
+    BookingComponent,
+    ContactComponent,
+    CalendarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
